e09c7928a7b103f532a4700b0d263f5374224fd1
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-11-26 15:37:25 +0100

    add conteggio lunghezza array e nuova lista con nuovo array

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,23 +7,41 @@ import tasks from './tasks'
 
 function App() {
   let tasksCompleted = []
+  let taskToComplete = []
   tasks.forEach((task) => {
     if (task.state === "completed") {
       tasksCompleted.push(task)
+    } else {
+      taskToComplete.push(task)
     }
   })
-  console.log(tasksCompleted)
+
   return (
     <>
       <header className="background-title">
         <h1 className="title"> Task Manager</h1>
       </header>
 
+      {/* Array con stato da completare */}
+      <h2>Current Tasks ({taskToComplete.length}) </h2>
+      <ul>
+        {taskToComplete.map((task) => (
+          <li key={task.id}>
+            <h3>{task.title} <span>{task.state}</span></h3>
+            <p>Priority: {task.priority}</p>
+            <p>Es. time: {task.estimatedTime}</p>
+          </li>
+        ))}
+      </ul>
 
+      <br />
+
+      {/* Array con stato completato */}
+      <h2>Current Tasks ({tasksCompleted.length}) </h2>
       <ul>
         {tasksCompleted.map((task) => (
           <li key={task.id}>
-            <h2>{task.title} </h2>
+            <h3>{task.title} <span>{task.state}</span> </h3>
             <p>Priority: {task.priority}</p>
             <p>Es. time: {task.estimatedTime}</p>
           </li>
