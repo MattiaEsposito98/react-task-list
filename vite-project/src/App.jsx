96af789ce980a9001f9c4d1e39fb078bec1517fb
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-11-26 15:23:53 +0100

    add for each per gli stati attivi e  map per creare gli elementi

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,11 +6,29 @@ import './tasks'
 import tasks from './tasks'
 
 function App() {
+  let tasksCompleted = []
+  tasks.forEach((task) => {
+    if (task.state === "completed") {
+      tasksCompleted.push(task)
+    }
+  })
+  console.log(tasksCompleted)
   return (
     <>
       <header className="background-title">
         <h1 className="title"> Task Manager</h1>
       </header>
+
+
+      <ul>
+        {tasksCompleted.map((task) => (
+          <li key={task.id}>
+            <h2>{task.title} </h2>
+            <p>Priority: {task.priority}</p>
+            <p>Es. time: {task.estimatedTime}</p>
+          </li>
+        ))}
+      </ul>
     </>
 
   )
